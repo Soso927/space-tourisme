c8184aa92e90770ced60442bc5a86947381ac63a
sofiane <sofianeagoumellah1@gmail.com>
2025-10-15 10:09:33 +0200

    correction du fichier app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,39 +21,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
-
-    // dans bootstrap/app.php
-
-$app = new Illuminate\Foundation\Application(
-    dirname(__DIR__)
-);
-
-// ici d’autres configurations…
-
-$app->withMiddleware(function (\Illuminate\Foundation\Configuration\Middleware $middleware) {
-    // Ajouter le middleware SetLocale dans le groupe web
-    $middleware->web(append: [
-        \App\Http\Middleware\SetLocale::class,
-        
-    ]);
-    class SetLocale
-{
-    public function handle($request, Closure $next)
-    {
-        // On récupère la langue stockée en session (fr ou en)
-        // Si aucune langue n’est trouvée, on utilise la langue par défaut du site
-        $locale = Session::get('locale', config('app.locale'));
-
-        // Laravel utilise cette langue pour toutes les traductions
-        App::setLocale($locale);
-
-        // On continue le chargement de la page
-        return $next($request);
-    }
-}
-    
-    // Si tu le veux globalement (pour toutes les routes), tu pourrais utiliser append() directement :
-     $middleware->append(\App\Http\Middleware\SetLocale::class);
-});
-
+    })
+    ->create();
